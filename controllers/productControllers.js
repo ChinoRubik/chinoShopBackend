6372462e43bb3e7aaa4c7b670bc49a0d3953338b
Cristian Hernandez <chinorubikguitar@hotmail.com>
2022-02-24 20:43:27 -0600

    getSale

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -452,7 +452,24 @@ const controller = {
                 });     
             })
         })
-    }
+    },
+
+    getSale: (req,res) => {
+        req.getConnection((err, conn) => {
+            if (err) return res.status(400).send({
+                message: err
+            });
+            conn.query("SELECT * FROM sales WHERE user_uuid = ?",[req.params.user_uuid],(err,rows) => {
+                if (err) return res.status(400).send({err})
+
+                return res.status(200).send({
+                    status: 'ok',
+                    rows
+                });
+            });
+        });
+    },
+
 }
 
 module.exports = controller;
