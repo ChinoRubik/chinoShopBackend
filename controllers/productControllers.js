fc2c7ba106bf86b270b9d7589e70b95c55b8623d
Cristian Hernandez <chinorubikguitar@hotmail.com>
2022-02-24 20:38:58 -0600

    give uuid sale

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -446,6 +446,7 @@ const controller = {
                 if (err) return res.status(400).send({err})
 
                 return res.status(200).send({
+                    uuid: obj.uuid,
                     status: 'ok',
                     rows
                 });     
